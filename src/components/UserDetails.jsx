c18b8c34ecf4aa8b9e1f7feb1a769f07825bfca6
ahartfelder <ahartfelder@gmail.com>
2022-07-19 22:23:03 -0300

    params-search params-dynamic routes

---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom"
+
+export default function UserDetails() {
+  const { userId } = useParams()
+  return (
+    <div>Details about user {userId}</div>
+  )
+}
